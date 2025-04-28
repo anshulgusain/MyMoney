0c5146d853e47cfe4339c5d09779a5888cda0de4
anshulgusain <anshulgusain99@gmail.com>
2025-04-28 10:49:18 +0530

    Components made

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function InputBox({label,placeholder}) {
+function InputBox({label,placeholder,onChange}) {
   return (
     <div>
       <div className='text-sm font-medium text-left py-2'>
         {label}</div>
-        <input placeholder={placeholder} className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 border rounded border-slate-200"></input>
+        <input onChange ={onChange}placeholder={placeholder} className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 border rounded border-slate-200"></input>
     </div>
   )
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import Appbar from "../components/Appbar"
+import Balance from "../components/Balance"
+import Users from "../components/User"
+import axios from "axios"
 
-function Dashboard() {
-  return (
-    <div>
-      
+const Dashboard = () => {
+const [balance,setBalance]=useState(0)
+    useEffect(() => {
+
+        const search=async()=>{
+            var response=   await axios.get("http://localhost:8080/api/v1/account/balance"
+                , {
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem("token")
+                    }
+                })
+                setBalance(parseInt(response.data.message))
+               
+                
+        }
+     search()
+    },[])
+    return <div className="bg-red">
+        <Appbar />
+        <div className="m-8"  >
+            <Balance value={balance} />
+            <Users />
+        </div>
     </div>
-  )
 }
 
 export default Dashboard
